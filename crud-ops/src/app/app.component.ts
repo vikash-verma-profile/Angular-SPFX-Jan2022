4c390cdcc74ca3b3f2fc8b859ba264952218f5ed
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-01-24 17:40:44 +0530

    crud application

--- crud-ops/src/app/app.component.ts
+++ crud-ops/src/app/app.component.ts
@@ -9,4 +9,13 @@ import { Customer } from './app.component.model';
 export class AppComponent {
   title = 'crud-ops';
   CustomerModel:Customer=new Customer();
+  CustomerModels:Array<Customer>=new Array<Customer>();
+  AddCustomer(){
+    //console.log(this.CustomerModel);
+    this.CustomerModels.push(this.CustomerModel);
+    
+    this.CustomerModel=new Customer(); //Clear the UI data
+    console.log(this.CustomerModels);
+
+  }
 }
