2ca5ada3bc66dd014b9fea96dc756d730745e43d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-01-25 12:32:08 +0530

    added Reactive form  driven  example

--- crud-ops/src/app/app.component.model.ts
+++ crud-ops/src/app/app.component.model.ts
@@ -7,10 +7,23 @@ export class Customer{
     formCustomerGroup:FormGroup;
 
     constructor(){
+
+        //This part was how we can add single validation
         var _builder=new FormBuilder();
         this.formCustomerGroup=_builder.group({});
         //control-->validated
         this.formCustomerGroup.addControl("CustomerNameControl",new FormControl('',Validators.required));
+
+
+        //This part was how we can add multiple validation
+        //required
+        //validate for 4 latter numeric
+        var validationcollection=[];
+        validationcollection.push(Validators.required);
+        validationcollection.push(Validators.pattern("^[0-9]{4,4}$"));
+
+        this.formCustomerGroup.addControl("CustomerCodeControl",new FormControl('',Validators.compose(validationcollection)));
+
         
     }
 }
