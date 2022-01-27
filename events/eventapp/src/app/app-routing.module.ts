8e3ec9e6493cd8eac4c7025ef6ecb2c1e366efe7
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-01-27 15:02:26 +0530

    events-applicatio

---
+++ events/eventapp/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EventsComponent } from "./events/events.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/events" },
+  {
+    path: "events",
+    component: EventsComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
